85fb020e0c3bd3c4d1e9c4a5b4205c5582d808de
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-03-26 14:26:10 -0600

    Start of Parser, test and run files

---
+++ ID3_Algorithm.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# Christian Ricardo Solís Cortés A01063685
+# Raúl Mar A00512318
+# Artificial Intelligence
+# Assignment: Decision Trees
+# Implementation of ID3 based on a dataset
+# -----------------------------------------------------------------------------
